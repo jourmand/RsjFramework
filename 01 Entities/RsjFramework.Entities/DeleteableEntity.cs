6e2c17c8932d16c721f0cb0fd3b0219f59e5ef07
Rasoul Jourmand <jourmand@live.com>
2021-05-29 12:40:05 +0430

    Update Dynamic.Core And Add DeleteableEntity

---
+++ 01 Entities/RsjFramework.Entities/DeleteableEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RsjFramework.Entities
+{
+    public class DeleteableEntity<T> : Entity<T>, IDeleteableEntity
+    {
+        public string CreatedBy { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public string LastModifiedBy { get; set; }
+
+        public DateTime? LastModified { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ 01 Entities/RsjFramework.Entities/IDeleteableEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RsjFramework.Entities
+{
+    public interface IDeleteableEntity
+    {
+        string CreatedBy { get; set; }
+        DateTime Created { get; set; }
+        string LastModifiedBy { get; set; }
+        DateTime? LastModified { get; set; }
+        bool IsDeleted { get; set; }
+    }
+}
